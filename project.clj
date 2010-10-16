fd3c02eec5097fd73cfffab32f04e3c63e99836a
James Aley <james.aley@gmail.com>
2010-10-16 15:52:13 +0100

    Started project for axis plug-in

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject lein-axis "1.0.0-SNAPSHOT"
+  :description "Simple Leiningen plug-in to speed up working with Apache Axis and WSDL files."
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+		 [axis/axis "1.4"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]]
+  )
---
+++ src/leiningen/axis.clj
@@ -0,0 +1,44 @@
+(ns leiningen.axis
+  (:use [clojure.java.shell :only [sh]]
+	[clojure.string :only [join]]
+	[leiningen.classpath :only [get-classpath]])
+  (:gen-class))
+
+(def *WSDL2Java-class* "org.apache.axis.wsdl.WSDL2Java")
+
+(defn- cmd
+  "Convert the [wsdl target-package] vector from project.clj to a WSDL2Java call."
+  [p [wsdl package]]
+  ["java" "-cp" (join java.io.File/pathSeparatorChar (get-classpath p)) *WSDL2Java-class*
+   "-o" (get p :java-source-path "src/java")
+   "-p" package
+   wsdl])
+
+(defn axis
+  "Use Apache Axis to generate Java classes for WSDL files.
+
+You will need to add Apache Axis to your project as a dependency, e.g.:
+...
+  :dependencies [[org.clojure/clojure \"1.2.0\"]
+                 [org.clojure/clojure-contrib \"1.2.0\"]
+		 [axis/axis \"1.4\"]]
+
+Then, to configure what WSDL files to use and where to put the generated
+source files:
+...
+  :java-source-path \"src/java\"
+  :axis [[\"src/wsdl/myservice.wsdl\" \"generated.myservice\"]
+	 [\"src/wsdl/myotherservice.wsdl\" \"generated.myotherservice\"]]
+
+Note that :java-source-path is used by the lein-javac plug-in, which is
+probably the easiest way to turn the Java code generated into compiled
+classes. lein-axis uses this setting as the target directory (for the
+root of the generated packages). src/java is used as a default if you
+don't provide this value.
+"
+  [project]
+  (doseq [cmd-out (map (comp (partial apply sh)
+			     (partial cmd project))
+		       (:axis project))]
+    (print (:err cmd))))
+
